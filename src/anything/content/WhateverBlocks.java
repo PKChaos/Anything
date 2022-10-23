73ed7b3ed829e8f184a7a511e1a642204285decc
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-23 10:16:28 +0800

    e

--- src/anything/content/WhateverBlocks.java
+++ src/anything/content/WhateverBlocks.java
@@ -3,16 +3,16 @@ package anything.content;
 import arc.struct.Seq;
 import mindustry.Vars;
 import anything.binding.Bind;
-import anything.world.blocks.UnitBlock;
+import anything.world.blocks.UnitBlockR;
 
 
 public class WhateverBlocks{
     
-    public static Seq<Bind<UnitBlock>> binds = new Seq<Bind<UnitBlock>>();
+    public static Seq<Bind<UnitBlockR>> binds = new Seq<Bind<UnitBlockR>>();
     
     public static void load(){
         Vars.content.units().each(u -> {
-            binds.add(new Bind<UnitBlock>(new UnitBlock(u)));
+            binds.add(new Bind<UnitBlockR>(new UnitBlockR(u)));
         });
     }
 }
